585f5871a780c3301f80a4e54e1430217db21567
nguyenletandai <nguyenletandai@gmail.com>
2019-03-25 09:24:49 +0700

    update man history

---
+++ Hosee/Hosee/HistoryTableViewCell.swift
@@ -0,0 +1,52 @@
+//
+//  TableViewCell.swift
+//  KiemTien2
+//
+//  Created by daicudu on 3/19/19.
+//  Copyright © 2019 daicudu. All rights reserved.
+//
+
+import UIKit
+@IBDesignable
+class HistoryTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var codeLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var taskLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var phoneLabel: UILabel!
+    @IBOutlet weak var priceLabel: UILabel!
+    @IBOutlet weak var containView: UIView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        setShadow()
+        
+        // Initialization code
+    }
+    
+    func setShadow() {
+        
+        containView.layer.shadowColor = UIColor.gray.cgColor
+        containView.layer.shadowOffset = CGSize(width: 0, height: 0)
+        containView.layer.shadowOpacity = 0.35
+        containView.layer.cornerRadius = 8
+        containView.layer.shadowRadius = 8
+    }
+
+    
+    override func prepareForReuse() {
+        codeLabel.text = ""
+        locationLabel.text = ""
+        taskLabel.text = ""
+        nameLabel.text = ""
+        phoneLabel.text = ""
+        priceLabel.text = ""
+    }
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+        // Configure the view for the selected state
+    }
+    
+}
